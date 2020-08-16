665f9d6bc0db6243aed56d74e4b2f1a3df98f695
salim semakula <salimsema@yahoo.com>
2020-08-16 22:52:41 +0300

    random sass mixin check

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,18 +21,7 @@ export default class About extends Component {
               dignissimos, placeat quia minima labore voluptatem cupiditate
               sequi exercitationem est velit nihillorem.
             </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-              laudantium unde, cupiditate accusantium quisquam ad? Rem enim fuga
-              dolore dicta ut perferendis aspernatur, quasi minus ipsum impedit
-              tempora quis laudantium! Ipsa officia ex hic doloremque
-              blanditiis, nostrum dolore ut neque corrupti ullam quos culpa
-              dolorum vitae voluptatibus, maiores sequi asperiores?Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Magnam optio modi
-              temporibus vero tempore? Earum, pariatur consectetur, quod
-              dignissimos, placeat quia minima labore voluptatem cupiditate
-              sequi exercitationem est velit nihillorem.
-            </p>
+
             <h4>Why you should hire us</h4>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
